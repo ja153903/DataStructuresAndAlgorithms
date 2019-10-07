9afa8df11a731254d532818f1ad2c04e00ded821
jaime <jaimeabbariao@gmail.com>
2019-10-07 11:44:00 -0400

    Modified pop() to have a return value

--- stack/Stack.js
+++ stack/Stack.js
@@ -12,7 +12,11 @@ class Stack {
       return;
     }
 
+    const returnValue = this.container[this.container.length - 1];
+
     this.container.splice(this.container.length - 1, 1);
+
+    return returnValue;
   }
 
   top() {
